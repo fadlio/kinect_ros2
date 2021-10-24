4c19927149358cc48020470274796c390470a779
Fernando de Lio <fernandodelio@gmail.com>
2021-10-23 22:34:53 -0300

    improved performance on the depth_cb

--- src/kinect_ros2_node.cpp
+++ src/kinect_ros2_node.cpp
@@ -1,7 +1,6 @@
 extern "C"
 {
   #include "libfreenect/libfreenect.h"
-// #include "libfreenect/libfreenect_sync.h"
 }
 
 #include <chrono>
@@ -19,6 +18,8 @@ extern "C"
 using namespace std::chrono_literals;
 
 static cv::Mat depth_image(cv::Mat::zeros(cv::Size(640, 480), CV_16UC1));
+static uint16_t * freenect_depth_pointer = nullptr;
+static bool image_flag;
 
 class KinectRos : public rclcpp::Node
 {
@@ -31,88 +32,94 @@ public:
 
     depth_pub_ = image_transport::create_publisher(this, "depth/image_raw");
 
-    int ret = freenect_init(&fn_ctx, NULL);
+    int ret = freenect_init(&fn_ctx_, NULL);
     if (ret < 0) {
       RCLCPP_ERROR(get_logger(), "ERROR INIT");
       rclcpp::shutdown();
     }
 
-    freenect_set_log_level(fn_ctx, FREENECT_LOG_FATAL);
-    freenect_select_subdevices(fn_ctx, FREENECT_DEVICE_CAMERA);
+    freenect_set_log_level(fn_ctx_, FREENECT_LOG_FATAL);
+    freenect_select_subdevices(fn_ctx_, FREENECT_DEVICE_CAMERA);
 
-    int num_devices = ret = freenect_num_devices(fn_ctx);
+    int num_devices = ret = freenect_num_devices(fn_ctx_);
     if (ret < 0) {
       RCLCPP_ERROR(get_logger(), "ERROR DEVICES");
       rclcpp::shutdown();
     }
     if (num_devices == 0) {
       printf("No device found!\n");
-      freenect_shutdown(fn_ctx);
+      freenect_shutdown(fn_ctx_);
       rclcpp::shutdown();
     }
-    freenect_device * fn_dev;
-    ret = freenect_open_device(fn_ctx, &fn_dev, 0);
+    ret = freenect_open_device(fn_ctx_, &fn_dev_, 0);
     if (ret < 0) {
-      freenect_shutdown(fn_ctx);
+      freenect_shutdown(fn_ctx_);
       RCLCPP_ERROR(get_logger(), "ERROR OPEN");
       rclcpp::shutdown();
     }
     ret =
       freenect_set_depth_mode(
-      fn_dev, freenect_find_depth_mode(
+      fn_dev_, freenect_find_depth_mode(
         FREENECT_RESOLUTION_MEDIUM,
         FREENECT_DEPTH_MM));
     if (ret < 0) {
-      freenect_shutdown(fn_ctx);
+      freenect_shutdown(fn_ctx_);
       RCLCPP_ERROR(get_logger(), "ERROR SET DEPTH");
       rclcpp::shutdown();
     }
-    freenect_set_depth_callback(fn_dev, depth_cb);
-    ret = freenect_start_depth(fn_dev);
+    freenect_set_depth_callback(fn_dev_, depth_cb);
+    ret = freenect_start_depth(fn_dev_);
     if (ret < 0) {
-      freenect_shutdown(fn_ctx);
+      freenect_shutdown(fn_ctx_);
       RCLCPP_ERROR(get_logger(), "ERROR START DEPTH");
       rclcpp::shutdown();
     }
   }
 
-private:
-  static void depth_cb(freenect_device * dev, void *depth, uint32_t timestamp)
+  ~KinectRos()
   {
-    printf("cb\n");
-    cv::Mat img = cv::Mat(640, 480, CV_16UC1, depth);
+    RCLCPP_INFO(get_logger(), "STOPING KINECT");
+    freenect_stop_depth(fn_dev_);
+    freenect_stop_video(fn_dev_);
+    freenect_close_device(fn_dev_);
+    freenect_shutdown(fn_ctx_);
+  }
 
-    std::memcpy(depth_image.data, img.data, 640 * 480 * sizeof(uint16_t));
-    // for (int i = 0; i < 100; i++){
-    //   printf("%u", img.data[i]);
-    // }
-    // printf("\n");
-    // cv::imshow("Depth", img);
+private:
+
+  /* The freenect lib stores the depth data in a static region of the memory, so the best way to
+  create a cv::Mat is passing the pointer of that region, avoiding the need of copying the data
+  to a new cv::Mat. This way, the callback only used to set a flag that indicates that a new image
+  has arrived. The flag is unset when a msg is published */
+  static void depth_cb(freenect_device * dev, void * depth_ptr, uint32_t timestamp)
+  {
+    if (image_flag) {
+      return;
+    }
+
+    if (freenect_depth_pointer != (uint16_t *)depth_ptr) {
+      depth_image = cv::Mat(480, 640, CV_16UC1, depth_ptr);
+      freenect_depth_pointer = (uint16_t *)depth_ptr;
+    }
+
+    image_flag = true;
   }
 
   void timer_callback()
   {
-    auto tick = now().nanoseconds();
-    freenect_process_events(fn_ctx);
-    // RCLCPP_INFO(get_logger(), "freenect ns: '%li'", now().nanoseconds() - tick);
+    freenect_process_events(fn_ctx_);
 
-    // static IplImage * image = 0;
-    // static char * data = 0;
-    // if (!image) {image = cvCreateImageHeader(cvSize(640, 480), 16, 1);}
-    // unsigned int timestamp;
-    // if (freenect_sync_get_depth((void **)&data, &timestamp, 0, FREENECT_DEPTH_11BIT)) {
-    //   return;
-    // }
-    // cvSetData(image, data, 640 * 2);
-    // return image;
+    if (image_flag) {
+      auto msg = cv_bridge::CvImage(std_msgs::msg::Header(), "mono16", depth_image).toImageMsg();
+      depth_pub_.publish(msg);
 
-    tick = now().nanoseconds();
-    // auto msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", depth_image_).toImageMsg();
-    // depth_pub_.publish(msg);
-    // RCLCPP_INFO(get_logger(), "Publish ns: '%li'", now().nanoseconds() - tick);
-    cv::imshow("Depth", depth_image);
+      cv::imshow("Depth", depth_image);
+      cv::waitKey(1);
+      image_flag = false;
+    }
   }
-  freenect_context * fn_ctx;
+  freenect_context * fn_ctx_;
+  freenect_device * fn_dev_;
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
 
